3b09874e14179cf0823cc1ab56659420c328b0d2
Conner Crosby <conner@cavcrosby.tech>
2021-08-28 20:31:04 -0400

    Add seed job-dsl

---
+++ job-dsl.groovy
@@ -0,0 +1,8 @@
+freeStyleJob ('seed') {
+    
+    /*
+    #   A barebone Jenkins job to be used with the Job DSL plugin to quickly create
+    #   other jobs as necessary for Jenkins job development/testing.
+    */
+
+}
